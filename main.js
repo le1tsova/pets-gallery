c3dd71336bc7e834c51f807fd458001b2a054323
Kate Levantsova <kate.levantsova@gmail.com>
2019-06-30 14:08:41 +0300

    И вновь продолжается бой

--- main.js
+++ main.js
@@ -1,33 +1,118 @@
 "use strict";
 
-const list = document.querySelector(".nav");
-
-fetchData("http://localhost:3000/cats", function(catsData) {
-  buildListCats(catsData, list);
-});
-
 function fetchData(url, callback, headerName, headerValue) {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", url);
 
-  if (headerName != undefined && headerValue != undefined) {
+  if (headerName && headerValue) {
     xhr.setRequestHeader(headerName, headerValue);
   }
-  xhr.send();
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState !== 4) {
       return;
     }
     let catsData = xhr.responseText;
+
     try {
       catsData = JSON.parse(catsData);
     } catch {
-      callback(undefined);
+      catsData = undefined;
     }
 
     callback(catsData);
   };
+
+  xhr.send();
+}
+
+function makeDummyErr(container) {
+  const dummy = document.createElement("p");
+  dummy.textContent = "Извините, ошибка в данных";
+  dummy.className = "dummy--error";
+  container.append(dummy);
+}
+
+function convertGenderToString(gender) {
+  return gender === "male" ? "Мальчик" : "Девочка";
+}
+
+function displayCatInfo(container, info) {
+  container.textContent = "";
+  if (!info) {
+    makeDummyErr(container);
+    return;
+  }
+
+  const headName = document.createElement("h1");
+  headName.textContent = info.payload.name;
+  const pGender = document.createElement("p");
+  pGender.textContent = convertGenderToString(info.payload.gender);
+  const pAge = document.createElement("p");
+  pAge.textContent = "Лет: " + info.payload.age;
+  container.appendChild(headName, pGender, pAge);
+  container.appendChild(pGender);
+  container.appendChild(pAge);
+}
+
+function makeDummyForComments(container) {
+  const dummy = document.createElement("p");
+  dummy.textContent = "Здесь еще нет ни одного комментария";
+  dummy.className = "dummy";
+  container.append(dummy);
+}
+
+function displayComments(container, dataComments) {
+  container.textContent = "";
+  const headerComments = document.createElement("h3");
+  headerComments.textContent = "Коммментарии";
+  container.append(headerComments);
+
+  if (!dataComments.payload) {
+    makeDummyErr(container);
+    return;
+  }
+
+  const listComments = dataComments.payload;
+
+  if (!listComments.comments.length) {
+    makeDummyForComments(container);
+    return;
+  }
+
+  listComments.comments.forEach(element => {
+    const divCommment = document.createElement("div");
+    divCommment.className = "comment__item";
+    const autor = document.createElement("p");
+    autor.className = "comment__author";
+    autor.textContent = element.author;
+
+    const textComment = document.createElement("p");
+    textComment.className = "comment__text";
+    textComment.textContent = element.content;
+    divCommment.appendChild(autor);
+    divCommment.appendChild(textComment);
+    container.appendChild(divCommment);
+  });
+}
+
+function displayCatPhoto(container, url) {
+  container.textContent = "";
+
+  if (!url.payload) return;
+
+  const photo = document.createElement("img");
+  photo.setAttribute("src", url.payload);
+  photo.className = "userpic__image";
+  container.appendChild(photo);
+}
+
+function currentItem() {
+  const anyLink = document.querySelectorAll(".nav__link");
+  anyLink.forEach(element => {
+    element.classList.remove("actual");
+  });
+  event.target.classList.add("actual");
 }
 
 const more = document.querySelector(".more");
@@ -58,8 +143,7 @@ function buildListCats(dataList, container) {
 
   container.appendChild(ulCats);
 
-  const linCats = document.querySelector(".nav__list");
-  linCats.addEventListener("click", function() {
+  ulCats.addEventListener("click", function() {
     fetchData(
       "http://localhost:3000/cats/" + event.target.getAttribute("data-id"),
       function(catsData) {
@@ -86,131 +170,13 @@ function buildListCats(dataList, container) {
 
     currentItem(event);
   });
-
-  function currentItem() {
-    const anyLink = document.querySelectorAll(".nav__link");
-    for (let i = 0; i < anyLink.length; i++) {
-      let a = anyLink[i];
-      a.classList.remove("actual");
-    }
-
-    event.target.classList.add("actual");
-  }
 }
 
-function displayCatInfo(container, info) {
-  container.textContent = "";
-  if (!info) {
-    makeDummyErr(container);
-    return;
-  }
+const list = document.querySelector(".nav");
 
-  const headName = document.createElement("h1");
-  headName.textContent = info.payload.name;
-  const pGender = document.createElement("p");
-  pGender.textContent = convertGenderToString(info.payload.gender);
-  const pAge = document.createElement("p");
-  pAge.textContent = "Лет: " + info.payload.age;
-  container.appendChild(headName, pGender, pAge);
-  container.appendChild(pGender);
-  container.appendChild(pAge);
-}
-
-function convertGenderToString(gender) {
-  if (gender === "male") {
-    return "Мальчик ";
-  } else return "Девочка ";
-}
-
-function displayComments(container, dataComments) {
-  container.textContent = "";
-  const headerComments = document.createElement("h3");
-  headerComments.textContent = "Коммментарии";
-  container.append(headerComments);
-
-  if (!dataComments.payload) {
-    makeDummyErr(container);
-    return;
-  }
-
-  const listComments = dataComments.payload;
-
-  if (listComments.comments.length === 0) {
-    makeDummyForComments(container);
-    return;
-  }
-
-  listComments.comments.forEach(element => {
-    const divCommment = document.createElement("div");
-    divCommment.className = "comment__item";
-    const autor = document.createElement("p");
-    autor.className = "comment__author";
-    autor.textContent = element.author;
-
-    const textComment = document.createElement("p");
-    textComment.className = "comment__text";
-    textComment.textContent = element.content;
-    divCommment.appendChild(autor);
-    divCommment.appendChild(textComment);
-    container.appendChild(divCommment);
-  });
-}
-
-function makeDummyForComments(container) {
-  const dummy = document.createElement("p");
-  dummy.textContent = "Здесь еще нет ни одного комментария";
-  dummy.className = "dummy";
-  container.append(dummy);
-}
-
-function makeDummyErr(container) {
-  const dummy = document.createElement("p");
-  dummy.textContent = "Извините, ошибка в данных";
-  dummy.className = "dummy--error";
-  container.append(dummy);
-}
-
-function displayCatPhoto(container, url) {
-  container.textContent = "";
-
-  if (!url.payload) return;
-
-  var photo = document.createElement("img");
-  photo.setAttribute("src", url.payload);
-  photo.className = "userpic__image";
-  container.appendChild(photo);
-}
-
-const form = document.querySelector(".form");
-
-form.addEventListener("submit", sendNewCat);
-
-function sendNewCat() {
-  event.preventDefault();
-  const xhr = new XMLHttpRequest();
-  const body = JSON.stringify({
-    name: document.getElementById("name").value,
-    age: document.getElementById("age").value,
-    gender: document.getElementById("gender").value
-  });
-
-  xhr.open("POST", "http://localhost:3000/cats");
-  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-  xhr.send(body);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState !== 4) {
-      return;
-    }
-    let newCat = xhr.responseText;
-
-    try {
-      newCat = JSON.parse(newCat);
-    } catch {
-      replyToUser(undefined);
-    }
-    replyToUser(newCat, xhr.status);
-  };
-}
+fetchData("http://localhost:3000/cats", function(catsData) {
+  buildListCats(catsData, list);
+});
 
 function replyToUser(answer, status) {
   if (!answer) {
@@ -230,3 +196,34 @@ function replyToUser(answer, status) {
     alert(answer.message);
   }
 }
+
+function sendNewCat() {
+  event.preventDefault();
+  const xhr = new XMLHttpRequest();
+  const body = JSON.stringify({
+    name: document.getElementById("name").value,
+    age: document.getElementById("age").value,
+    gender: document.getElementById("gender").value
+  });
+
+  xhr.open("POST", "http://localhost:3000/cats");
+  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    let newCat = xhr.responseText;
+
+    try {
+      newCat = JSON.parse(newCat);
+      replyToUser(newCat, xhr.status);
+    } catch {
+      replyToUser(undefined);
+    }
+  };
+  xhr.send(body);
+}
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", sendNewCat);
